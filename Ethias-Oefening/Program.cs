e655aa64d5a664926cbe49c95ae9c2c7edd8a837
WoutAlenPXL <wout.alen@student.pxl.be>
2024-09-24 10:29:20 +0200

    les 2
    Oefeningen af behalve tryprase ding

--- Ethias-Oefening/Program.cs
+++ Ethias-Oefening/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Net.Cache;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -20,8 +22,14 @@ namespace Ethias_Oefening
              *  - Zorg er voor dat je groet meerdere regels lang is
              *  - Druk de groet af, zodra het programma start
              * 
-             */
-            string groet = "?";
+               */
+            string groet = " ---------------------------------------------- \n " +
+                           "|                                             | \n " +
+                           "|  Welkom Bij Ethias Verzekeringsmaatschapij  | \n " +
+                           "|                                             | \n " +
+                           " ----------------------------------------------";
+            Console.WriteLine(groet);
+            
 
 
 
@@ -37,6 +45,25 @@ namespace Ethias_Oefening
              *  - Telefoonnummer
              */
 
+            Console.WriteLine("Wat is u voornaam?");
+            string Voornaam = Console.ReadLine();
+
+            Console.WriteLine("Wat is u achternaam?");
+            string Achternaam = Console.ReadLine();
+
+            Console.WriteLine("Hoe oud bent u?");
+            string leeftijdInTekst = Console.ReadLine();
+            int Leeftijd = Convert.ToInt32(leeftijdInTekst);
+
+            Console.WriteLine("Bent u een man? Y/N");
+            string AntwoordOpVraag = Console.ReadLine();
+            bool IsMan = "Y" == AntwoordOpVraag;
+
+
+            Console.WriteLine("Wat is u telefoonnummer?");
+            string Telefoonnummer = Console.ReadLine();
+
+
 
 
             /*
@@ -47,6 +74,14 @@ namespace Ethias_Oefening
              * Gebruik string interpolatie om de variabelen te injecteren in een 
              * samenvatting. Zorg er voor dat je samenvatting uitlijning bevat.
              */
+            Console.WriteLine($"De gegevens van de klant: \n" +
+                              $"Volledige naam: {Voornaam} {Achternaam}\n" +
+                              $"Leeftijd: {Leeftijd}\n" +
+                              $"Is geslacht man? {IsMan}\n" +
+                              $"Telefoonnummer: {Telefoonnummer}");
+            Console.ReadLine();
+
+
 
 
 
